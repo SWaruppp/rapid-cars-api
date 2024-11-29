b774161a867e1e14e937def8db256f37f7970978
SWaruppp <155567332+SWaruppp@users.noreply.github.com>
2024-11-29 11:14:39 +0530

    Update index.ts

--- utils/index.ts
+++ utils/index.ts
@@ -50,13 +50,13 @@ export async function fetchCars(filters: FilterProps) {
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
 
-  // // Set the required headers for the API request
-  // const response = await fetch(
-  //   `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
-  //   {
-  //     headers: headers,
-  //   }
-  // );
+  // Set the required headers for the API request
+  const response = await fetch(
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
+    {
+      headers: headers,
+    }
+  );
 
   // Parse the response as JSON
   const result = await response.json();
